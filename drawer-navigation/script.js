71f564e76fdfad277099dec3388b4dd97ef6a216
Kotaro Sugawara <kotarella1110@gmail.com>
2020-10-29 10:38:39 +0900

    part 2-6

--- drawer-navigation/script.js
+++ drawer-navigation/script.js
@@ -11,6 +11,10 @@
   const scrollbarFixTargets = document.querySelectorAll(".js-scrollbarFix");
   let scrollbarFix = false;
 
+  const scrollableTarget = drawer.querySelector(".js-scrollable");
+
+  let touchY = null;
+
   // 現在の状態（開いていたらtrue）
   let drawerOpen = false;
 
@@ -41,6 +45,7 @@
   }
 
   function onClickOpenButton() {
+    // アニメーション開始時にスクロール位置を固定
     activateScrollLock();
     openDrawer();
   }
@@ -59,11 +64,20 @@
     rootElement.classList.remove(scrollLockModifier);
   }
 
+  /**
+  * ナビを開いているときにメインコンテンツがスクロールされないようにする対策
+  * ナビを開いているときにはメインコンテンツのスクロール位置を固定し、閉じたら元に戻す、という処理を行う
+  */
+
   function onTransitionendDrawer(event) {
+    // transitionend イベントはプロパティごとにも発生するため、対象とするプロパティのトランジション終了であるかどうかを判定
+    // 状態が遷移するドロワー本体のプロパティは visibility のみ
     if (event.target !== drawer || event.propertyName !== "visibility") {
       return;
     }
     if (!drawerOpen) {
+      // アニメーション終了時にスクロール位置を固定
+      // 閉じるボタンのクリックイベントに結び付けることができないため、トランジション（遷移アニメーション）終了時の transitionend イベントにイベントハンドラを追加
       deactivateScrollLock();
     }
   }
@@ -77,7 +91,10 @@
   }
 
   function addScrollbarWidth() {
+    // スクロールバーの幅（ブラウザの表示領域からhtmlの幅を引いた数値）
+    // `html { overflow:auto }` の場合、ブラウザがリサイズされたときは再度取得し直す必要がある
     const scrollbarWidth = window.innerWidth - rootElement.clientWidth;
+    // スクロールバーの幅を取得するとき、その値が 0 なら margin-right を追加しないようにする
     if (!scrollbarWidth) {
       scrollbarFix = false;
       return;
@@ -94,8 +111,60 @@
     addScrollbarMargin("");
   }
 
+  /**
+   * iOS Safari の対策
+   * iOS Safariでは `html { overflow:hidden }` であってもスクロールできてしまう
+   */
+
+  function onTouchStart(event) {
+    // タッチのポイントが複数かどうかを判定
+    // この処理によってピンチイン/アウトなどマルチタッチ動作を妨げることがなくなる
+    if (event.targetTouches.length > 1) {
+      return;
+    }
+    touchY = event.targetTouches[0].clientY;
+  }
+
+  function onTouchMove(event) {
+    if (event.targetTouches.length > 1) {
+      return;
+    }
+    // touchstart時と現在の差分から、スクロール方向を得る
+    // 正：上方向へスクロール
+    // 負：下方向へスクロール
+    const touchMoveDiff = event.targetTouches[0].clientY - touchY;
+
+    // スクロールさせたい対象のスクロール位置が一番上の場合、上方向のスクロールをキャンセルする
+    if (scrollableTarget.scrollTop === 0 && touchMoveDiff > 0) {
+      event.preventDefault();
+      return;
+    }
+
+    // スクロールさせたい対象のスクロール位置が一番下の場合、下方向へのスクロールをキャンセルする
+    if (targetTotallyScrolled(scrollableTarget) && touchMoveDiff < 0) {
+      event.preventDefault();
+    }
+  }
+
+  // ターゲット要素がすべてスクロールし終わっているかどうかを判定
+  function targetTotallyScrolled(element) {
+    return element.scrollHeight - element.scrollTop <= element.clientHeight;
+  }
+
+  // オーバーレイ部分は常にスクロール動作をキャンセルする
+  function onTouchMoveBackdrop(event) {
+    if (event.targetTouches.length > 1) {
+      return;
+    }
+    event.preventDefault();
+  }
+
   openButton.addEventListener("click", onClickOpenButton, false);
   closeButton.addEventListener("click", onClickCloseButton, false);
   backdrop.addEventListener("click", onClickCloseButton, false);
   drawer.addEventListener("transitionend", onTransitionendDrawer, false);
+
+  scrollableTarget.addEventListener("touchstart", onTouchStart, false);
+  scrollableTarget.addEventListener("touchmove", onTouchMove, false);
+  backdrop.addEventListener("touchmove", onTouchMoveBackdrop, false);
 })();
